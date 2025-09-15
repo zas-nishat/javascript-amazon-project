3f4611f8212d676087921a6041e78551f51d9042
Nishat <104446439+zas-nishat@users.noreply.github.com>
2025-09-15 16:49:29 +0600

    add to cart page updated with js

--- js/carts.js
+++ js/carts.js
@@ -1,4 +1,25 @@
-export const carts = [];
+export const carts = [
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+
+    },
+    {
+
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+
+    }];
 
 
 // Add to carts function
